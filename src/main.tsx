69cbe3635ee505bc3ae786e600e81b74e1b559f5
Paarth Madan <paarth.mdn@gmail.com>
2024-06-05 18:07:36 -0400

    Add tempo init

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import { TempoDevtools } from "tempo-devtools";
+
+TempoDevtools.init();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tempoVitePlugin as tempo } from "tempo-devtools"
 
+const conditionalPlugins = [];
+
+if (process.env.TEMPO) {
+  conditionalPlugins.push('tempo-devtools/dist/babel-plugin');
+}
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tempo()],
+  plugins: [react({
+    babel: {
+      plugins: conditionalPlugins,
+    }
+  }), tempo()],
   resolve: {
     preserveSymlinks: true,
   }
