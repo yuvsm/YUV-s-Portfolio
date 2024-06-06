00177dabbe9396f121c5813d733cf602db7d3c41
petergok <petergok@hotmail.com>
2024-06-06 16:44:53 -0400

    Fix conditional plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins = [];
 
 // @ts-ignore
 if (process.env.TEMPO) {
-  conditionalPlugins.push('tempo-devtools/swc');
+  conditionalPlugins.push(["tempo-devtools/swc", {}]);
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    plugins: [["tempo-devtools/swc", {}]],
-  }),
-  tempo()
-],
+  plugins: [
+    react({
+      plugins: conditionalPlugins,
+    }),
+    tempo(),
+  ],
   resolve: {
     preserveSymlinks: true,
-  }
-})
+  },
+});
