73e6b16e0a80fe9c705fca9b698b44e0874ac9e3
petergok <petergok@hotmail.com>
2024-06-06 15:15:10 -0400

    Version of vite-swc that works

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
 import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins = [];
 
 // @ts-ignore
 if (process.env.TEMPO) {
-  conditionalPlugins.push('tempo-devtools/dist/babel-plugin');
+  conditionalPlugins.push('tempo-devtools/swc');
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
-    babel: {
-      plugins: conditionalPlugins,
-    }
+    plugins: [["tempo-devtools/swc", {}]],
   }),
   tempo()
 ],
