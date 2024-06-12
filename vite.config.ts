d2aedb2eae394280a57603145f87d34cc59ec8c2
petergok <petergok@hotmail.com>
2024-06-12 13:36:04 -0400

    Updated back to babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins = [];
 
 // @ts-ignore
 if (process.env.TEMPO) {
-  conditionalPlugins.push(["tempo-devtools/swc", {}]);
+  conditionalPlugins.push('tempo-devtools/dist/babel-plugin');
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      plugins: conditionalPlugins,
+      babel: {
+        plugins: conditionalPlugins,
+      }
     }),
     tempo(),
   ],
