523f1b436f1378fb3cc1714f9235854e7442f2e5
petergok <petergok@hotmail.com>
2025-01-16 14:38:47 -0500

    Update code for env

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins: [string, Record<string, any>][] = [];
-let basePath = process.env.VITE_BASE_PATH || "/";
 
 // @ts-ignore
 if (process.env.TEMPO === "true") {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
-    // In Tempo, we don't want to use any base path
-  basePath = "/";
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: basePath,
+  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
