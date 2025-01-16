b882942afe69441f65644f3d56464c9c6388ce90
petergok <petergok@hotmail.com>
2025-01-16 13:27:13 -0500

    Don't use base path in tempo

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,18 @@ import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins: [string, Record<string, any>][] = [];
+let basePath = process.env.VITE_BASE_PATH || "/";
 
 // @ts-ignore
 if (process.env.TEMPO === "true") {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
+    // In Tempo, we don't want to use any base path
+  basePath = "/";
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: basePath,
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
